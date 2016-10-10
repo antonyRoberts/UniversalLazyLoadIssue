e0ccad22317286b6561e89a48af88be0b8886132
antonyRoberts <antonyrobertsgit@gmail.com>
2016-10-10 11:34:10 -0400

    Remove moduleId

--- src/app/features/admin/components/siteStats.component.ts
+++ src/app/features/admin/components/siteStats.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'site-stats-main',
     templateUrl: '../templates/siteStats.component.html'
 })
